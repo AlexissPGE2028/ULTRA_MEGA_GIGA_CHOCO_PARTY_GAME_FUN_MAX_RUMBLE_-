e6a862467eee831f636230548eee23d99de45440
Adrien <adrien.gaillard-lafargue@epitech.eu>
2023-12-03 08:06:55 +0100

    Adding main functionnalities for real

--- FlappyChoco/pipe.py
+++ FlappyChoco/pipe.py
@@ -7,17 +7,19 @@ class Pipe_bot(pygame.sprite.Sprite):
         super().__init__()
         self.sprite = pygame.image.load("Assets/pipe_bot.png")
         self.sprite.set_colorkey([0, 0, 0])
-        self.sprite = pygame.transform.scale(self.sprite, (150, 450))
+        self.sprite = pygame.transform.scale(self.sprite, (100, 450))
         self.rect = self.sprite.get_rect()
+        self.rect.topleft = (x, y)
         self.pos = [x, y]
-        self.speed = 1
+        self.speed = 7
 
     def move(self):
         self.pos[0] -= self.speed
         if self.pos[0] < -150:
             self.pos[0] = 1000
-            self.pos[1] = randint(150, 400)
-            self.speed += 1
+            self.pos[1] = randint(200, 450)
+            #self.speed += 1
+        self.rect.topleft = (self.pos[0], self.pos[1])
 
 class Pipe_top(pygame.sprite.Sprite):
 
@@ -25,14 +27,16 @@ class Pipe_top(pygame.sprite.Sprite):
         super().__init__()
         self.sprite = pygame.image.load("Assets/pipe_top.png")
         self.sprite.set_colorkey([0, 0, 0])
-        self.sprite = pygame.transform.scale(self.sprite, (150, 450))
+        self.sprite = pygame.transform.scale(self.sprite, (100, 450))
         self.rect = self.sprite.get_rect()
+        self.rect.topleft = (x, y)
         self.pos = [x, y]
-        self.speed = 1
+        self.speed = 7
 
     def move(self, y_bot):
         self.pos[0] -= self.speed
         if self.pos[0] < -150:
             self.pos[0] = 1000
-            self.pos[1] = y_bot - 600
-            self.speed += 1
+            self.pos[1] = y_bot - 675
+            #self.speed += 1
+        self.rect.topleft = (self.pos[0], self.pos[1])

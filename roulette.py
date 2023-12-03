7466e15ae6bcbe285c5cd928f8ef5ee46c9633c1
sacha <sacha.vedovatti@epitech.eu>
2023-12-03 23:38:35 +0100

    Editing roulette for Choco Jump V1.0

--- roulette.py
+++ roulette.py
@@ -2,12 +2,14 @@ import os
 import sys
 import random
 
-game = random.randint(1, 3)
+game = random.randint(1, 4)
 
 print(game)
-if (game == 1 or sys.argv[1] == "flappy"):
+if (game == 1):
     os.system("python3 FlappyChoco/main.py")
 elif (game == 2):
     os.system("./SHOKOBANG/MY_CHOCOBANG SHOKOBANG/map/Level_1")
 elif (game == 3):
     print("pack openning soon...")
+elif (game == 4):
+    os.system("./'Choco Jump'/'the game'/'Super Choco Bros.x86_64'")
